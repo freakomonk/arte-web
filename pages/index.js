e22a3eaf3cfaa9c332e21e437c6344eed6e01233
Abhimithra Karthikeya Surabhi <freakomonk@Abhimithras-MacBook-Air.local>
2021-07-06 20:22:56 +0530

    Basic changes

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ allPosts }) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Arte Origin</title>
         </Head>
         <Container>
           <Intro />
